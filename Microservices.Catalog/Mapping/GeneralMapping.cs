34f0f5851c3c9f7c1e765bb62f73e86e247cd552
ilayda <ilaydaozken@gmail.com>
2023-08-03 14:25:29 +0300

    Add project files.

---
+++ Microservices.Catalog/Mapping/GeneralMapping.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Microservices.Catalog.Dtos.CategoryDtos;
+using Microservices.Catalog.Models;
+
+namespace Microservices.Catalog.Mapping
+{
+    public class GeneralMapping:Profile
+    {
+        public GeneralMapping()
+        {
+            CreateMap<Category,ResultCategoryDto>().ReverseMap();
+        }
+    }
+}
